fbbcd92aaed0ca4d0fb12357485eafa59bb7a2c4
Keyur6610 <120013936+Keyur6610@users.noreply.github.com>
2023-03-04 11:43:42 +0530

    Add files via upload

---
+++ PRINT_INT_BY_USER.kt
@@ -0,0 +1,9 @@
+/* Print an Integer (Entered by the User) */  
+
+fun main(args: Array<String>) {
+  
+  println("Enter Num: ")
+  var num = Integer.valueOf(readLine())
+  println("Your Entered Num: $num")
+  
+  }
