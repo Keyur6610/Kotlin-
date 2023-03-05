77f1348f0b49be765d396ffb7a31ad531f59a765
Keyur6610 <120013936+Keyur6610@users.noreply.github.com>
2023-03-05 23:25:24 +0530

    Add files via upload

---
+++ EVEN_OR_ODD_NUM.kt
@@ -0,0 +1,18 @@
+/* Kotlin Program to Check Whether a Number is Even or Odd */
+
+
+fun main(args: Array<String>) {
+  
+  println("Enter Number: ")
+  var num=Integer.valueOf(readLine())
+  
+  if(num%2==0){
+    
+    println("\nNumber is Even")
+    
+  }else{
+    
+    println("\nNumber is Odd")
+  }
+  
+}
