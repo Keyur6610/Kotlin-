dc2530aa389e22cb894e282d47742f7bc2370df1
Keyur6610 <120013936+Keyur6610@users.noreply.github.com>
2023-03-07 12:43:56 +0530

    Add files via upload

---
+++ FLOAT_MUL.kt
@@ -0,0 +1,17 @@
+import java.util.Scanner
+
+fun main() {
+    
+    var sc = Scanner(System.`in`)
+    
+    println("Enter Number: ")
+    var a = sc.nextFloat()
+   
+    println("Enter Number: ")
+    var b = sc.nextFloat()
+   
+    var c = a*b
+    
+    println("Multiplication Of $a × $b = $c")
+    
+}
