e16a70fc463afdfb8b982caaea1b3678fe69b6fe
Keyur6610 <120013936+Keyur6610@users.noreply.github.com>
2023-03-21 14:57:32 +0530

    Add files via upload

---
+++ ARRAY.kt
@@ -0,0 +1,14 @@
+fun main ()
+{
+
+  var a = arrayOf (1, 2, 3, 4, 5, "Hello", "World")
+  
+     /* for(i in 0..6){
+       println(a[i])
+       } */
+       
+    for (b in a)
+    {
+      println ("$b")
+    }
+}
