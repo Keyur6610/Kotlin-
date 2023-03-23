8be1ddf11752edcdfea1fc887439ee405e9f7bf8
Keyur6610 <120013936+Keyur6610@users.noreply.github.com>
2023-03-23 16:18:26 +0530

    Add files via upload

---
+++ LOOPS.kt
@@ -0,0 +1,38 @@
+fun main() {
+
+ println("Enter Num: ")
+ var a = Integer.valueOf(readLine())
+ 
+ for(i in 1..10){
+     println("$a × $i = ${a*i}")
+ }
+    
+/*  for(i in 1..10){
+        println(i)
+    }
+    
+    for(i in 1..10 step 2){
+        println(i)
+    }
+    
+    for(i in 1 until 10){
+        println(i)
+    } 
+    
+    for(i in 10 downTo 1){
+        println(i)
+    } 
+    
+    
+    do{
+        println("a = $a")
+        a++
+    }while(a<=10) */
+    
+    
+   /* while(a<=10){
+        println("a = $a")
+        a++
+    }  */
+    
+}
